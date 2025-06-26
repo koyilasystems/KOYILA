a3cbd6cf40e0c253b941b0dd923ab245577edcf5
KOYILASYSTEMS <koyilasystems@gmail.com>
2025-06-26 12:48:30 +0530

    Update Register.jsx

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "${process.env.REACT_APP_API_BASE_URL}/api/v1/user/register",
+        "https://koyila-backend.onrender.com/api/v1/user/register",
         { name, phone, email, role, password },
         {
           headers: {
