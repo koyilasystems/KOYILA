82f46d3298993e2f59d11a3036985e6176f49791
KOYILASYSTEMS <koyilasystems@gmail.com>
2025-06-26 16:11:30 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "https://koyila-backend.onrender.com/api/v1/user/getuser",
+          "${process.env.REACT_APP_API_URL}/api/v1/user/getuser",
           {
             withCredentials: true,
           }
