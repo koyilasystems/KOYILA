cb519c64f28463ff94852ee98e7c24271103a17c
KOYILASYSTEMS <koyilasystems@gmail.com>
2025-06-26 11:40:32 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ dotenv.config({path: "./config/config.env"});
 
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.https://koyila-frontend.onrender.com],
     methods: ["GET","POST","DELETE","PUT"],
     credentials: true,
 }));
