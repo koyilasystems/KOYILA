6a1c0d6f76a0609945bc84bfb3037b3a036c34b9
KOYILASYSTEMS <koyilasystems@gmail.com>
2025-06-26 11:51:13 +0530

    Update JobDetails.jsx

--- frontend/src/components/Job/JobDetails.jsx
+++ frontend/src/components/Job/JobDetails.jsx
@@ -12,7 +12,7 @@ const JobDetails = () => {
 
   useEffect(() => {
     axios
-    .get(`${import.meta.env.VITE_API_BASE_URL}/api/v1/job/${id}`, {
+    .get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/job/${id}`, {
       withCredentials: true,
     })
       .then((res) => {
