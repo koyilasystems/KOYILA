b1407c3f622a0c1eb1d774c0501c894a24f7511b
KOYILASYSTEMS <koyilasystems@gmail.com>
2025-06-26 17:04:20 +0530

    Update PostJob.jsx

--- frontend/src/components/Job/PostJob.jsx
+++ frontend/src/components/Job/PostJob.jsx
@@ -31,7 +31,7 @@ const PostJob = () => {
     }
     await axios
       .post(
-        "${process.env.REACT_APP_API_URL}/api/v1/job/post",
+        "https://koyila-backend.onrender.com/api/v1/job/post",
         fixedSalary.length >= 4
           ? {
               title,
